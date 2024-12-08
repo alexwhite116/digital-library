3f13e0270a05c11a7b6fb7804788fefc3b65f19f
Alex <alexwhite116@gmail.com>
2024-12-08 02:10:24 +0000

    Created Book and Library class. Added functionality to create books with some details; to convert books to list/dict format; to list titles of all books in library; CLI interface to display all titles of books in library, to add new books, and to exit; included traps to prevent blank/non-integer input.

---
+++ librarystructures.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self,
+                 title: str,
+                 author: str,
+                 genre: str,
+                 pages: int,
+                 publication_year: int):
+        self.title: str = title
+        self.author: str = author
+        self.genre: str = genre
+        self.pages: int = pages
+        self.publication_year: int = publication_year
+
+
+    def to_dict(self) -> dict[str, str | int]:
+        book_dictionary = {
+            'title' : self.title,
+            'author' : self.author,
+            'genre' : self.genre,
+            'pages' : self.pages,
+            'publication_year' : self.publication_year
+        }
+        return book_dictionary
+
+    def to_list(self) -> list[str | int | None]:
+        book_list = [
+            self.title,
+            self.author,
+            self.genre,
+            self.pages,
+            self.publication_year
+        ]
+        return book_list
+
+class Library:
+    def __init__(self):
+        self.books: list[Book] = []
+
+    def title_list(self) -> str:
+        titles = ''
+        for book in self.books:
+            titles += f"{book.title}\n"
+        return titles
+
+    def add_book(self, book: Book):
+        self.books.append(book)
---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+from librarystructures import Library, Book
+import csv
+
+def create_book(title: str,
+                author: str,
+                genre: str,
+                pages: int,
+                publication_year: int):
+    book = Book(title, author, genre, pages, publication_year)
+    return book
+
+def get_string_input(prompt: str) -> str:
+    user_input = input(prompt)
+    if len(user_input) == 0:
+        print(prompt[:-2] + ' cannot be empty. Please try again.')
+        return get_string_input(prompt)
+
+    return user_input
+
+def get_int_input(prompt: str) -> int:
+    try:
+        user_input = int(input(prompt))
+    except ValueError as e:
+        print(f"{e}: Please enter an integer.")
+        return get_int_input(prompt)
+
+    return user_input
+
+def main():
+    library = Library()
+    while True:
+        user_input = input("DISPLAY / NEW / EXIT: ").lower()
+
+        if user_input == 'exit':
+            print('Thank you! Exiting now.')
+            sys.exit()
+
+        if user_input == 'display':
+            print(library.title_list())
+
+        if user_input == 'new':
+            title = get_string_input("Book Title: ")
+            author = get_string_input("Book Author: ")
+            genre = get_string_input("Book Genre: ")
+            pages = get_int_input("# of Pages: ")
+            publication_year = get_int_input("Publication year: ")
+            book = create_book(title, author, genre, pages, publication_year)
+            library.add_book(book)
+
+if __name__ == '__main__':
+    main()
