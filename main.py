05e2ab00bb3a766dc12d68cc85eaf9ec935fda03
Alex <alexwhite116@gmail.com>
2024-12-08 21:08:23 +0000

    Added functionality to save library to .csv file.

--- main.py
+++ main.py
@@ -22,10 +22,29 @@ def get_int_input(prompt: str) -> int:
     try:
         user_input = int(input(prompt))
     except ValueError as e:
-        print(f"{e}: Please enter an integer.")
+        print(f"Error: {e} --- Please enter an integer.")
         return get_int_input(prompt)
 
     return user_input
+
+def get_book_details() -> list[str | int]:
+    details: list[str | int] = [get_string_input("Book Title: "),
+                                get_string_input("Book Author: "),
+                                get_string_input("Book Genre: "),
+                                get_int_input("# of Pages: "),
+                                get_int_input("Publication year: ")]
+    return details
+
+def save_library(filename: str, library: Library) -> None:
+    if filename[-4:] == '.csv':
+        filename = filename[:-4]
+    libraryList: list[dict] = [book.to_dict() for book in library.books]
+    print('Saving...')
+    with open(f'{filename}.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=libraryList[0].keys())
+        writer.writeheader()
+        writer.writerows(libraryList)
+    print(f'Saved successfully to {filename}.csv')
 
 def main():
     library = Library()
@@ -41,17 +60,14 @@ def main():
             continue
 
         if user_input == 'new':
-            title = get_string_input("Book Title: ")
-            author = get_string_input("Book Author: ")
-            genre = get_string_input("Book Genre: ")
-            pages = get_int_input("# of Pages: ")
-            publication_year = get_int_input("Publication year: ")
-            book = create_book(title, author, genre, pages, publication_year)
+            # details = get_book_details()
+            details = ['Great Expectations', 'Charles Dickens', 'Classical Fiction', 479, 1861]
+            book = create_book(*details) # unpack details
             library.add_book(book)
             continue
 
         if user_input == 'save':
-            # ...
+            save_library('books', library)
             continue
 
 
